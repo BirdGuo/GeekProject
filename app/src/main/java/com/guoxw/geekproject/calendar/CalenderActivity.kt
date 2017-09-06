4def76f093f8f4b16e29f17901621d149f3a43a3
gxw2 <gxw603004002@126.com>
2017-09-06 18:28:42 +0800

    添加老黄历页面

---
+++ app/src/main/java/com/guoxw/geekproject/calendar/CalenderActivity.kt
@@ -0,0 +1,18 @@
+package com.guoxw.geekproject.calendar
+
+import com.guoxw.geekproject.R
+import com.guoxw.geekproject.base.BaseActivity
+
+class CalenderActivity : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_calender
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initListener() {
+    }
+
+}
---
+++ app/src/main/java/com/guoxw/geekproject/calendar/bean/Calendar.kt
@@ -0,0 +1,11 @@
+package com.guoxw.geekproject.calendar.bean
+
+/**
+ * Created by guoxw on 2017/9/6 0006.
+ * @auther guoxw
+ * @date 2017/9/6 0006
+ * @desciption
+ * @package com.guoxw.geekproject.calendar.bean
+ */
+class Calendar {
+}
