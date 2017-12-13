b6525950b151a126314f0de66ef4225cf486ddbe
gxw2 <gxw603004002@126.com>
2017-12-13 17:56:30 +0800

    添加地图定位

--- app/src/main/java/com/guoxw/geekproject/MapActivity.kt
+++ app/src/main/java/com/guoxw/geekproject/MapActivity.kt
@@ -1,6 +1,7 @@
 package com.guoxw.geekproject
 
 import android.os.Bundle
+import com.amap.api.maps.model.MyLocationStyle
 import com.guoxw.geekproject.base.BaseToolbarActivity
 import kotlinx.android.synthetic.main.activity_map.*
 import kotlinx.android.synthetic.main.include_toolbar.*
@@ -13,7 +14,34 @@ class MapActivity : BaseToolbarActivity() {
         amap_map.onCreate(savedInstanceState)
         setToolBar(tb_toolbar_base, "地图")
 
+        initLocation()
+    }
 
+    fun initLocation() {
+        //初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。
+        //（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        var myLocationStyle = MyLocationStyle()
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW)//只定位一次。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE)//定位一次，且将视角移动到地图中心点。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW)//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE)//连续定位、且将视角移动到地图中心点，地图依照设备方向旋转，定位点会跟随设备移动。（1秒1次定位）
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE)//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+//以下三种模式从5.1.0版本开始提供
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER)//连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER)//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE_NO_CENTER)//连续定位、蓝点不会移动到地图中心点，地图依照设备方向旋转，并且蓝点会跟随设备移动。
+        myLocationStyle.interval(2000)//设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        //设置定位蓝点style
+        amap_map.map.myLocationStyle = myLocationStyle
+        //设置默认定位按钮是否显示，非必需设置。
+        amap_map.map.uiSettings.isMyLocationButtonEnabled = true
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        amap_map.map.isMyLocationEnabled = true
+
+        amap_map.map.setOnMyLocationChangeListener { location ->
+            //从location对象中获取经纬度信息，地址描述信息，建议拿到位置之后调用逆地理编码接口获取（获取地址描述数据章节有介绍）
+        }
 
     }
 
---
+++ app/src/main/java/com/guoxw/geekproject/utils/FileUtil.kt
@@ -0,0 +1,12 @@
+package com.guoxw.geekproject.utils
+
+/**
+ * @auther guoxw
+ * @date 2017/12/13 0013
+ * @package com.guoxw.geekproject.utils
+ * @desciption
+ */
+object FileUtil {
+
+
+}
