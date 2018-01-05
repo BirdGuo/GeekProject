767f321c034cb6fd2a51bffa911278f7bfe7bf33
gxw2 <gxw603004002@126.com>
2018-01-05 17:52:09 +0800

    问题未解决，添加callback

--- app/src/main/cpp/uninstallListenerJniLib.cpp
+++ app/src/main/cpp/uninstallListenerJniLib.cpp
@@ -12,27 +12,42 @@ Java_com_guoxw_geekproject_jniutil_UninstallUtil_callUnInstallListener(JNIEnv *e
     LOGD("-----------");
     const char *path_str = env->GetStringUTFChars(path, JNI_FALSE);
     pid_t pid = fork();
-
-//    char *pidc = (char *) pid;
-//
-//    LOGD(pidc);
-
     if (pid < 0) {
         LOGD("克隆失败");
     } else if (pid > 0) {
         LOGD("父进程");
     } else {
         LOGD("子进程");
+        /**
+         * 在内核中创建 inotify 子系统的一个实例，成功的话将返回一个文件描述符，失败则返回 -1。
+         * 就像其他系统调用一样，如果 inotify_init() 失败，请检查 errno 以获得诊断信息。
+         */
         int fuileDescript = inotify_init();
-        LOGD("子进程1");
+        /**
+         * 用于添加监视器。
+         * 每个监视器必须提供一个路径名和相关事件的列表（每个事件由一个常量指定，比如 IN_MODIFY）。
+         * 要监控多个事件，只需在事件之间使用逻辑操作符或 — C 语言中的管道线（|）操作符。
+         * 如果 inotify_add_watch() 成功，该调用会为已注册的监视器返回一个惟一的标识符；
+         * 否则，返回 -1。使用这个标识符更改或删除相关的监视器。
+         */
         int watch = inotify_add_watch(fuileDescript, path_str, IN_DELETE_SELF);
-        LOGD("子进程2");
+        /**
+         * 说明：malloc 向系统申请分配指定size个字节的内存空间。返回类型是 void* 类型。
+         * void* 表示未确定类型的指针。C,C++规定，void* 类型可以强制转换为任何其它类型的指针。
+         * 这个在MSDN上可以找到相关的解释，具体内容如下：
+         * malloc returns a void pointer to the allocated space, or NULL if there is insufficient memory available.
+         * To return a pointer to a type other than void, use a type cast on the return value.
+         * The storage space pointed to by the return value is guaranteed to be suitably aligned for storage of any type of object.
+         * If size is 0, malloc allocates a zero-length item in the heap and returns a valid pointer to that item. Always check the return from malloc,
+         * even if the amount of memory requested is small.
+         */
         void *p = malloc(sizeof(struct inotify_event));
-        LOGD("子进程3");
+
         read(fuileDescript, p, sizeof(struct inotify_event));
-        LOGD("子进程4");
+        /**
+         * 删除一个监视器
+         */
         inotify_rm_watch(fuileDescript, watch);
-        LOGD("子进程5");
 //        if (versionSDK < 17) {
 //            LOGD("------小于17-----");
 //            execlp("am", "am", "start", "-a", "android.intent.action.VIEW", "-d",
---
+++ app/src/main/java/com/guoxw/geekproject/jniutil/JNICallbackUtil.kt
@@ -0,0 +1,15 @@
+package com.guoxw.geekproject.jniutil
+
+/**
+ * @auther guoxw
+ * @date 2018/1/5 0005
+ * @package com.guoxw.geekproject.jniutil
+ * @desciption
+ */
+object JNICallbackUtil {
+
+    init {
+        System.loadLibrary("native-lib")
+    }
+
+}
