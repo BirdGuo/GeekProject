45c42122b7fb18db3e77858c17a3eb99764b486e
gxw2 <gxw603004002@126.com>
2018-03-06 17:55:33 +0800

    添加服务IP

--- app/src/main/java/com/guoxw/geekproject/socket/ServerActivity.kt
+++ app/src/main/java/com/guoxw/geekproject/socket/ServerActivity.kt
@@ -2,6 +2,7 @@ package com.guoxw.geekproject.socket
 
 import com.guoxw.geekproject.R
 import com.guoxw.geekproject.jniutil.ServerUtil
+import com.guoxw.geekproject.utils.NetWorkUtil
 import kotlinx.android.synthetic.main.activity_server.*
 import kotlinx.android.synthetic.main.include_toolbar.*
 
@@ -9,6 +10,8 @@ class ServerActivity : BaseSocketActivity() {
 
     override fun initSocketUI() {
         setToolBar(tb_toolbar_base, "服务端")
+
+        tv_server_ip.text = NetWorkUtil.getIPAddress(this)
     }
 
     override fun getSocketUILayout(): Int = R.layout.activity_server
--- app/src/main/java/com/guoxw/geekproject/utils/NetWorkUtil.kt
+++ app/src/main/java/com/guoxw/geekproject/utils/NetWorkUtil.kt
@@ -2,13 +2,17 @@ package com.guoxw.geekproject.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
+import java.net.Inet4Address
+import java.net.NetworkInterface
+import java.net.SocketException
 
 /**
-* @auther guoxw
-* @date 2017/12/6 0006
-* @package ${PACKAGE_NAME}
-* @desciption
-*/
+ * @auther guoxw
+ * @date 2017/12/6 0006
+ * @package ${PACKAGE_NAME}
+ * @desciption
+ */
 object NetWorkUtil {
 
     /**
@@ -22,4 +26,49 @@ object NetWorkUtil {
         return networkInfo.isConnected
 
     }
+
+    fun getIPAddress(context: Context): String? {
+        val info = (context
+                .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
+        if (info != null && info.isConnected) {
+            if (info.type == ConnectivityManager.TYPE_MOBILE) {//当前使用2G/3G/4G网络
+                try {
+                    //Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
+                    val en = NetworkInterface.getNetworkInterfaces()
+                    while (en.hasMoreElements()) {
+                        val intf = en.nextElement()
+                        val enumIpAddr = intf.inetAddresses
+                        while (enumIpAddr.hasMoreElements()) {
+                            val inetAddress = enumIpAddr.nextElement()
+                            if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                                return inetAddress.getHostAddress()
+                            }
+                        }
+                    }
+                } catch (e: SocketException) {
+                    e.printStackTrace()
+                }
+
+            } else if (info.type == ConnectivityManager.TYPE_WIFI) {//当前使用无线网络
+                val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+                val wifiInfo = wifiManager.connectionInfo
+                return intIP2StringIP(wifiInfo.ipAddress)
+            }
+        }
+        return null
+    }
+
+    /**
+     * 将得到的int类型的IP转换为String类型
+     *
+     * @param ip
+     * @return
+     */
+    fun intIP2StringIP(ip: Int): String {
+        return (ip and 0xFF).toString() + "." +
+                (ip shr 8 and 0xFF) + "." +
+                (ip shr 16 and 0xFF) + "." +
+                (ip shr 24 and 0xFF)
+    }
+
 }
