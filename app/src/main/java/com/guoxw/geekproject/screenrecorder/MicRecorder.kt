ed28b714e1bf86b985b409419f7a3eae67b5cfe8
gxw2 <gxw603004002@126.com>
2018-04-02 16:49:17 +0800

    修改MicRecorder

--- app/src/main/java/com/guoxw/geekproject/screenrecorder/MicRecorder.kt
+++ app/src/main/java/com/guoxw/geekproject/screenrecorder/MicRecorder.kt
@@ -1,11 +1,13 @@
 package com.guoxw.geekproject.screenrecorder
 
 import android.media.*
-import android.media.MediaCodec.INFO_OUTPUT_FORMAT_CHANGED
+import android.media.MediaCodec.*
 import android.os.*
 import android.os.Build.VERSION_CODES.N
-import android.util.Log
+import android.util.SparseLongArray
 import com.guoxw.geekproject.utils.LogUtil
+import java.io.IOException
+import java.nio.ByteBuffer
 import java.util.*
 import java.util.concurrent.atomic.AtomicBoolean
 
@@ -41,6 +43,8 @@ class MicRecorder : Encoder {
     private val MSG_STOP = 4
     private val MSG_RELEASE = 5
 
+    private val LAST_FRAME_ID = -1
+    private val mFramesUsCache = SparseLongArray(2)
 
     constructor(config: AudioEncodeConfig) {
         this.mEncoder = AudioEncoder(config)
@@ -51,11 +55,13 @@ class MicRecorder : Encoder {
         this.mRecordThread = HandlerThread(TAG)
     }
 
+    @Throws(IOException::class)
     override fun prepare() {
         val myLooper = Objects.requireNonNull(Looper.myLooper(), "Should prepare in HandlerThread")
         mCallbackDelegate = CallbackDelegate(myLooper, mCallback!!)
         mRecordThread.start()
         mRecordHandler = RecordHandler(mRecordThread.looper)
+        mRecordHandler!!.sendEmptyMessage(MSG_PREPARE)
     }
 
     override fun stop() {
@@ -86,16 +92,58 @@ class MicRecorder : Encoder {
         override fun handleMessage(msg: Message?) {
             when (msg!!.what) {
                 MSG_PREPARE -> {
+                    var r: AudioRecord? = createAudioRecord(mSampleRate, mChannelConfig, mFormat)
+                    if (r == null) {
+                        LogUtil.e(TAG, "create audio record failure")
+                    } else {
+                        r.startRecording()
+                        mMic = r
+                    }
+
+                    try {
+                        mEncoder.prepare()
+                    } catch (e: Exception) {
+                        mCallbackDelegate!!.onError(this@MicRecorder, e)
+                    }
                 }
                 MSG_FEED_INPUT -> {
+
+                    if (!mForceStop.get()) {
+                        val index: Int = pollInput()
+                        if (VERBOSE) {
+                            LogUtil.d(TAG, "audio encoder returned input buffer index=".plus(index))
+                        }
+                        if (index >= 0) {
+                            feedAudioEncoder(index)
+                            if (!mForceStop.get())
+                                sendEmptyMessage(MSG_DRAIN_OUTPUT)
+                        } else {
+                            if (VERBOSE) {
+                                LogUtil.i(TAG, "try later to poll input buffer")
+                            }
+                            sendEmptyMessageDelayed(MSG_FEED_INPUT, mPollRate.toLong())
+                        }
+                    }
                 }
                 MSG_DRAIN_OUTPUT -> {
+                    offerOutput()
+                    pollInputIfNeed()
                 }
                 MSG_RELEASE_OUTPUT -> {
+                    mEncoder.releaseOutputBuffer(msg.arg1)
+                    mMuxingOutputBufferIndices.poll()
+                    pollInputIfNeed()
                 }
                 MSG_STOP -> {
+                    mMic?.stop()
+                    mEncoder.stop()
                 }
                 MSG_RELEASE -> {
+                    if (mMic != null) {
+                        mMic!!.release()
+                        mMic = null
+                    }
+                    mEncoder.release()
                 }
             }
         }
@@ -109,7 +157,7 @@ class MicRecorder : Encoder {
                 val index = mEncoder.mEncoder!!.dequeueOutputBuffer(info, 1)
                 if (VERBOSE) LogUtil.d(TAG, "audio encoder returned output buffer index=" + index)
                 if (index == INFO_OUTPUT_FORMAT_CHANGED) {
-                    mCallbackDelegate.onOutputFormatChanged(mEncoder, mEncoder.mEncoder!!.outputFormat)
+                    mCallbackDelegate!!.onOutputFormatChanged(mEncoder, mEncoder.mEncoder!!.outputFormat)
                 }
                 if (index < 0) {
                     info.set(0, 0, 0, 0)
@@ -117,7 +165,7 @@ class MicRecorder : Encoder {
                     break
                 }
                 mMuxingOutputBufferIndices.offer(index)
-                mCallbackDelegate.onOutputBufferAvailable(mEncoder, index, info)
+                mCallbackDelegate!!.onOutputBufferAvailable(mEncoder, index, info)
 
             }
         }
@@ -129,9 +177,45 @@ class MicRecorder : Encoder {
         fun pollInputIfNeed() {
             if (mMuxingOutputBufferIndices.size <= 1 && !mForceStop.get()) {
                 removeMessages(MSG_FEED_INPUT)
+                sendEmptyMessageDelayed(MSG_FEED_INPUT, 0)
             }
         }
 
+    }
+
+    /**
+     * should waiting all output buffer disapper queue input buffer
+     */
+    private fun feedAudioEncoder(index: Int) {
+
+        if (index < 0 || mForceStop.get())
+            return
+        val r: AudioRecord = Objects.requireNonNull(mMic, "maybe release")!!
+        //声明一个boolean
+        val eos: Boolean = r.recordingState == AudioRecord.RECORDSTATE_STOPPED
+        val frame: ByteBuffer = mEncoder.getInputBuffer(index)
+        var offset: Int = frame.position()
+        var limit: Int = frame.limit()
+
+        var read = 0
+        if (!eos) {
+            read = r.read(frame, limit)
+            LogUtil.d(TAG, "Read frame data size ".plus(read)
+                    .plus(" for index ").plus(index)
+                    .plus(" buffer : ").plus(offset)
+                    .plus(", ").plus(limit))
+            if (read < 0) {
+                read = 0
+            }
+        }
+        var pstTs: Long = calculateFrameTimestamp(read shl 3)
+        var flags = BUFFER_FLAG_KEY_FRAME
+        if (eos) {
+            flags = BUFFER_FLAG_END_OF_STREAM
+        }
+
+        LogUtil.d(TAG, "Feed codec index=$index, presentationTimeUs=$pstTs, flags=$flags")
+        mEncoder.queueInputBuffer(index, offset, read, pstTs, flags)
     }
 
     inner class CallbackDelegate : Handler {
@@ -178,10 +262,40 @@ class MicRecorder : Encoder {
         if (VERBOSE) {
             LogUtil.i(TAG, "created AudioRecord $record, MinBufferSize= $minBytes")
             if (Build.VERSION.SDK_INT >= N) {
-                LogUtil.d(TAG, " size in frame " + record.bufferSizeInFrames)
+                LogUtil.d(TAG, " size in frame ".plus(record.bufferSizeInFrames))
             }
         }
         return record
+    }
+
+    private fun calculateFrameTimestamp(totalBits: Int): Long {
+        val samples = totalBits shr 4
+        var frameUs: Long = mFramesUsCache.get(samples, -1)
+
+        if (frameUs == -1L) {
+            frameUs = (samples * 1000_000 / mChannelsSampleRate).toLong()
+            mFramesUsCache.put(samples, frameUs)
+        }
+
+        var timeUs: Long = SystemClock.elapsedRealtimeNanos() / 1000
+
+        timeUs -= frameUs
+        var currentUs: Long?
+        var lastFrameUs: Long = mFramesUsCache.get(LAST_FRAME_ID, -1)
+        if (lastFrameUs == -1L) {
+            currentUs = timeUs
+        } else {
+            currentUs = lastFrameUs
+        }
+
+        LogUtil.i(TAG, "count samples pts: $currentUs, time pts: $timeUs, samples: $samples")
+
+        if (timeUs - currentUs >= (frameUs shl 1)) {
+            currentUs = timeUs
+        }
+
+        mFramesUsCache.put(LAST_FRAME_ID, currentUs + frameUs)
+        return currentUs
     }
 
 }
