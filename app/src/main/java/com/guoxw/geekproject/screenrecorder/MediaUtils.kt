50edd819a7a132ace8cb7e983fc457be3117a4a2
gxw2 <gxw603004002@126.com>
2018-04-03 17:05:27 +0800

    添加工具类

---
+++ app/src/main/java/com/guoxw/geekproject/screenrecorder/MediaUtils.kt
@@ -0,0 +1,10 @@
+package com.guoxw.geekproject.screenrecorder
+
+/**
+ * @auther guoxw
+ * @date 2018/4/3 0003
+ * @package com.guoxw.geekproject.screenrecorder
+ * @desciption
+ */
+class MediaUtils {
+}
